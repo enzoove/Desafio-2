0718249319db30d5ba945a5a3421ffafa4397e18
enzoove <83042412+enzoove@users.noreply.github.com>
2022-10-05 21:46:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+
+class Contenedor
+{
+    constructor(nombreArchivo)
+    {
+        this.nombreArchivo = nombreArchivo;
+    }
+    async validateExistFile()
+    {
+        try {
+          await fs.promises.stat(this.nombreArchivo);
+          return 1;
+        } catch (err) {
+          console.error(`${this.nombreArchivo} no se encuentra en la ruta especificada. Creando archivo...`);
+          await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([]));
+          return 0;
+        }
+      }
+      async save()
+      {
+
+      }
+      async getById()
+      {
+
+      }
+      async getAll()
+      {
+        try{
+            const contenido = await fs.promises.readFile(this.nombreArchivo,'utf-8');
+            return JSON.parse(contenido);
+        }catch(err){
+            console.log(err);
+        }
+      }
+    async deleteById(del_Id)
+    {
+
+    }
+    async deleteAll()
+    {
+        try{
+            await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([]));
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+}
+
+const productos = new Contenedor('productos.json');
+productos.validateExistFile();
+console.log(productos.getAll());
+productos.deleteAll();
