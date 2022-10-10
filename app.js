b2b3235021f8d83289f4d600574c2ab943af2d76
enzoove <83042412+enzoove@users.noreply.github.com>
2022-10-10 20:12:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { stringify } = require('querystring');
 
 class Contenedor
 {
@@ -48,10 +49,39 @@ class Contenedor
     const data = JSON.stringify(arrayProducts, null, '\t');
     await fs.promises.writeFile(this.fileName,data);
     {
-      console.log( `${newProduct.title} ha sido guardado correctament!` );
+      console.log( `${newProduct.title} ha sido guardado correctamente!` );
     }
     }
-}
+    async getByID(idClave)
+    {
+      const content = await fs.promises.readFile( this.fileName, 'utf-8' );
+      const arrayProducts = JSON.parse(content);
+
+      const indice = arrayProducts.findIndex((unProducto) => unProducto.ID === idClave );
+      if( indice<0 )
+      {
+        throw new Error('El producto no existe');
+      }
+      return arrayProducts[indice];
+    }
+    async deleteById(idClave)
+    {
+      const content = await fs.promises.readFile( this.fileName, 'utf-8' );
+      const arrayProducts = JSON.parse(content);
+
+      const indice = arrayProducts.findIndex((unProducto) => unProducto.ID === idClave );
+      if( indice<0 )
+      {
+        return;
+      }
+      arrayProducts.splice(indice,1);
+      console.log('Producto eliminado correctamnete');
+
+      const data = JSON.stringify(arrayProducts, null, '\t');
+      await fs.promises.writeFile(this.fileName,data);
+    }
+    
+  }
 
 const file = new Contenedor('productos.json');
 
@@ -59,6 +89,7 @@ const file = new Contenedor('productos.json');
 //getAll: Por alguna razon me dice "json is not defined"
 //console.log( file.getAll() );
 
+//deleteAll
 //file.deleteAll();
 
 //save
@@ -67,4 +98,11 @@ const producto_1 ={
   price: 22,
   thumbnail: "https://www.distribuidoradales.com/wp-content/uploads/2020/10/PPM0003_Mesa-de-trabajo-1_Mesa-de-trabajo-1_Mesa-de-trabajo-1_Mesa-de-trabajo-1.jpg"
 }
-file.save(producto_1);
+//file.save(producto_1);
+
+//getByID: "PromiseState: 'pending'"
+const prod = file.getByID(2);
+console.log( prod );
+
+//deleteByID
+//file.deleteById(2);
