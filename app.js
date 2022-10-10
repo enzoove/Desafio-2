2f5c636e1aad2fec1edd0cda870962064e10e1e0
enzoove <83042412+enzoove@users.noreply.github.com>
2022-10-10 18:22:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,54 +3,40 @@ const path = require('path');
 
 class Contenedor
 {
-    constructor(nombreArchivo)
+  constructor(fileName)
+  {
+    this.fileName = fileName;
+  }
+  
+  async getAll()
+  {
+    try
     {
-        this.nombreArchivo = nombreArchivo;
+      const content = await fs.promises.readFile(this.fileName,'utf-8');
+      const products = JSON.parse(content);
+
+      return products;
+    }catch(err)
+    {
+      console.log('No se encontro ningun producto', err);
     }
-    async validateExistFile()
+  }
+  async deleteAll()
+  {
+    try
     {
-        try {
-          await fs.promises.stat(this.nombreArchivo);
-          return 1;
-        } catch (err) {
-          console.error(`${this.nombreArchivo} no se encuentra en la ruta especificada. Creando archivo...`);
-          await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([]));
-          return 0;
-        }
-      }
-      async save()
-      {
-
-      }
-      async getById()
-      {
-
-      }
-      async getAll()// por alguna razon me aparece este mensaje No hay ningún depurador disponible; no se puede enviar "variables".
-      {
-        try{
-            const contenido = await fs.promises.readFile(this.nombreArchivo,'utf-8');
-            return JSON.parse(contenido);
-        }catch(err){
-            console.log(err);
-        }
-      }
-    async deleteById(del_Id)
+      const products = [];
+      const data = JSON.stringify(products, null, '\t');
+      await fs.promises.writeFile(this.fileName, data);
+    }catch(err)
     {
-
+      console.log(err);
     }
-    async deleteAll()
-    {
-        try{
-            await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([]));
-        }catch(err){
-            console.error(err);
-        }
-    }
-
+  }
 }
 
-const productos = new Contenedor('productos.json');
-productos.validateExistFile();
-console.log(productos.getAll());
-productos.deleteAll();
+const contenedor = new Contenedor('productos.json');
+
+console.log( contenedor.getAll() );
+
+//contenedor.deleteAll();
